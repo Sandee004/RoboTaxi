3845c7273be848d379326498eca63cf155f57d01
Sandra Oyarekhua <102047643+Sandee004@users.noreply.github.com>
2025-05-19 07:20:17 +0100

    Added padding to the footer

--- taxicoin/src/components/Footer.tsx
+++ taxicoin/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="text-center text-sm py-5 bg-[#0cc0df] text-black">
+    <div className="text-center text-sm p-5 bg-[#0cc0df] text-black">
       <p className="pb-1">
         $Taxicoin has no association with Tesla robotaxi company. This token
         was created to honour Elonmusk's ambitious vision of fully autonomous
