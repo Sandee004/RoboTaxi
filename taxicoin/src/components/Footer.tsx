7eed158d7f4a8a667f92725ba56d861b3d473653
Sandra Oyarekhua <102047643+Sandee004@users.noreply.github.com>
2025-05-19 09:44:07 +0100

    Update Footer.tsx

--- taxicoin/src/components/Footer.tsx
+++ taxicoin/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className="text-center text-sm p-5 bg-[#0cc0df] text-black">
-      <p className="pb-1">
+      <p className="pb-6">
         $Taxicoin has no association with Tesla robotaxi company. This token
         was created to honour Elonmusk's ambitious vision of fully autonomous
         self-driving Robotaxi
